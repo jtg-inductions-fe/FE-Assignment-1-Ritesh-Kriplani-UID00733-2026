58794c6fa52b786b8f23aaee5226974bf280cfc8
riteshkriplani <ritesh.kriplani@joshtechnology.com>
2026-07-08 19:38:00 +0530

    [RK_A1_03]: clean up the code

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,6 @@
 import { initDropdown } from './modules/navigation.js';
 import { initMobileMenu } from './modules/mobileMenu.js';
 
-// ==========================================
-// SWIPER CAROUSEL INITIALIZATION
-// ==========================================
 document.addEventListener('DOMContentLoaded', () => {
    
     initDropdown();
