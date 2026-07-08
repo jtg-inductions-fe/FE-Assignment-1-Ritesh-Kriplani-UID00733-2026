bcbb7c68f68f9813d4777b36119f538f82512fb3
riteshkriplani <ritesh.kriplani@joshtechnology.com>
2026-07-08 23:57:58 +0530

    [RK_A1_03]: Fixed PR reviews

--- src/script/index.js
+++ src/script/index.js
@@ -2,7 +2,6 @@ import { initDropdown } from './modules/navigation.js';
 import { initMobileMenu } from './modules/mobileMenu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-   
     initDropdown();
     initMobileMenu();
 });
