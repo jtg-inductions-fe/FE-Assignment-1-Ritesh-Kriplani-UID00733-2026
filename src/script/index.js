2970f780002659c23f4f852648213d6d10ad0a3a
riteshkriplani <ritesh.kriplani@joshtechnology.com>
2026-07-09 23:39:30 +0530

    [RK_A1_01]: PR fixes done

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,6 @@
 import '../styles/main.scss';
 import { initDropdown } from './modules/navigation.js';
-import { initMobileMenu } from './modules/MoblieMenu.js';
+import { initMobileMenu } from './modules/mobileMenu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initDropdown();
