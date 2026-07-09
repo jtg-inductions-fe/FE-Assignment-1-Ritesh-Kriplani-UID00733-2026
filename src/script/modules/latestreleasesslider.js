f102f97057df2048662811d4855c35ab8d1b8fc8
riteshkriplani <ritesh.kriplani@joshtechnology.com>
2026-07-09 11:18:45 +0530

    [RK_A1_03]: Cleaned up the code

---
+++ src/script/modules/latestreleasesslider.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export function initLatestReleasesSlider() {
+    const sliderEl = document.querySelector('.latest-releases__slider');
+
+    if (!sliderEl) return;
+
+    new Swiper(sliderEl, {
+        modules: [Navigation, Pagination],
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 24,
+        initialSlide: 1,
+
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+                centeredSlides: false,
+            },
+        },
+    });
+}
